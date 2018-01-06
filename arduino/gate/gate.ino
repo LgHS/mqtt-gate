710d7c14c8a0846a1d1377803d994a9765ef1e9f
iooner <contact@iooner.be>
2018-01-06 18:57:51 +0100

    Initial gate client

---
+++ arduino/gate/gate.ino
@@ -0,0 +1,184 @@
+/*
+ *******************************************************************************
+ *
+ * Purpose: Example of using the Arduino MqttClient with EthernetClient.
+ * Project URL: https://github.com/monstrenyatko/ArduinoMqtt
+ *
+ *******************************************************************************
+ * Copyright Oleg Kovalenko 2017.
+ *
+ * Distributed under the MIT License.
+ * (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
+ *******************************************************************************
+ */
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = {
+  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
+};
+
+EthernetClient client;
+
+
+// Enable MqttClient logs
+#define MQTT_LOG_ENABLED 1
+// Include library
+#include <MqttClient.h>
+
+#include "config.h"
+
+#include "gate.pb.h"
+typedef be_lghs_gate_proto_GateOpenRequest GateOpenRequest;
+
+#include "pb.h"
+#include "pb_decode.h"
+#include "pb_encode.h"
+
+
+#define LOG_PRINTFLN(fmt, ...)  printfln_P(PSTR(fmt), ##__VA_ARGS__)
+#define LOG_SIZE_MAX 128
+void printfln_P(const char *fmt, ...) {
+  char buf[LOG_SIZE_MAX];
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf_P(buf, LOG_SIZE_MAX, fmt, ap);
+  va_end(ap);
+  Serial.println(buf);
+}
+
+
+MqttClient *mqtt = NULL;
+EthernetClient network;
+
+// ============== Object to supply system functions ================================
+class System: public MqttClient::System {
+public:
+  unsigned long millis() const {
+    return ::millis();
+  }
+};
+
+// ============== Setup all objects ============================================
+void setup() {
+  // Setup hardware serial for logging
+  Serial.begin(9600);
+
+  while (!Serial);
+
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+  } else {
+    printIPAddress();
+
+  }
+
+  // Setup MqttClient
+  MqttClient::System *mqttSystem = new System;
+  MqttClient::Logger *mqttLogger = new MqttClient::LoggerImpl<HardwareSerial>(Serial);
+  MqttClient::Network * mqttNetwork = new MqttClient::NetworkClientImpl<Client>(network, *mqttSystem);
+  //// Make 128 bytes send buffer
+  MqttClient::Buffer *mqttSendBuffer = new MqttClient::ArrayBuffer<128>();
+  //// Make 128 bytes receive buffer
+  MqttClient::Buffer *mqttRecvBuffer = new MqttClient::ArrayBuffer<128>();
+  //// Allow up to 2 subscriptions simultaneously
+  MqttClient::MessageHandlers *mqttMessageHandlers = new MqttClient::MessageHandlersImpl<2>();
+  //// Configure client options
+  MqttClient::Options mqttOptions;
+  ////// Set command timeout to 10 seconds
+  mqttOptions.commandTimeoutMs = 10000;
+  //// Make client object
+  mqtt = new MqttClient (
+    mqttOptions, *mqttLogger, *mqttSystem, *mqttNetwork, *mqttSendBuffer,
+    *mqttRecvBuffer, *mqttMessageHandlers
+  );
+}
+
+void printIPAddress()
+{
+  Serial.print("My IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print(".");
+  }
+
+  Serial.println();
+}
+
+void Reconnect() {
+  // Close connection if exists
+  network.stop();
+  // Re-establish TCP connection with MQTT broker
+  network.connect(BROKER_URL, BROKER_PORT);
+  // Start new MQTT connection
+  LOG_PRINTFLN("Connecting");
+  MqttClient::ConnectResult connectResult;
+  // Connect
+  {
+    MQTTPacket_connectData options = MQTTPacket_connectData_initializer;
+    options.MQTTVersion = 4;
+    options.clientID.cstring = CLIENT_ID;
+    options.cleansession = true;
+    options.keepAliveInterval = 15; // 15 seconds
+    MqttClient::Error::type rc = mqtt->connect(options, connectResult);
+    if (rc != MqttClient::Error::SUCCESS) {
+      LOG_PRINTFLN("Connection error: %i", rc);
+      return;
+    }
+  }
+}
+
+MqttClient::Message SendMqttMessage(char* topic, char* bytes, unsigned length) {
+  MqttClient::Message message;
+  message.qos = MqttClient::QOS0;
+  message.retained = false;
+  message.dup = false;
+  message.payload = bytes;
+  message.payloadLen = length;
+  mqtt->publish(topic, message);
+}
+
+void SendGateOpenRequest(const GateOpenRequest& request) {
+  /* This is the buffer where we will store our message. */
+  uint8_t buffer[128];
+  size_t message_length;
+  bool status;
+
+  /* Encode our message */
+  {
+    pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+    status = pb_encode(&stream, be_lghs_gate_proto_GateOpenRequest_fields, &request);
+    message_length = stream.bytes_written;
+
+    /* Then just check for any errors.. */
+    if (!status) {
+      printf("Encoding failed: %s\n", PB_GET_ERROR(&stream));
+      return;
+    }
+
+    SendMqttMessage("lghs/gate/" GATE_NUMBER "/open/request", buffer, message_length);
+  }
+}
+
+// ============== Main loop ====================================================
+void loop() {
+  // Check connection status
+  if (!mqtt->isConnected()) {
+    Reconnect();
+
+    // Add subscribe here if need
+  } else {
+    SendGateOpenRequest(GateOpenRequest {
+      1, // uint64_t cardId;
+      1, // uint64_t tokenLow;
+      1, // uint64_t tokenHigh;
+      1  // pb_callback_t pin;
+    });
+
+    // Idle for 30 seconds
+    mqtt->yield(30000L);
+  }
+}
