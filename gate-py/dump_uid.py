0489f2db98c78aaf064bf008b2f2a71af5eaefda
john <john@3kd.be>
2018-05-05 19:06:56 +0200

    add script to dump RFID card uids

---
+++ gate-py/dump_uid.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import time
+import OPi.GPIO as GPIO
+import MFRC522
+import signal
+import json
+
+import uids
+
+continue_reading = True
+
+
+def end_read(signal, frame):
+    global continue_reading
+    continue_reading = False
+
+
+def read_sector(mifare_reader, sector, key, uid):
+    status = mifare_reader.MFRC522_Auth(mifare_reader.PICC_AUTHENT1A, sector, key, uid)
+    if status != mifare_reader.MI_OK:
+        return None
+
+    return mifare_reader.MFRC522_Read(sector)
+
+
+def main():
+    global continue_reading
+
+    default_key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
+    mifare_reader = MFRC522.MFRC522()
+
+    last_card = None
+    last_rfid_read = 0
+
+    # This loop keeps checking for chips. If one is near it will get the UID and authenticate
+    while continue_reading:
+        now = time.time()
+
+        (status, tag_type) = mifare_reader.MFRC522_Request(mifare_reader.PICC_REQIDL)
+
+        if status != mifare_reader.MI_OK:
+            continue
+
+        (status, uid) = mifare_reader.MFRC522_Anticoll()
+
+        if now - last_rfid_read < 1:
+            last_rfid_read = now
+            continue
+
+        # If we have the UID, continue
+        if status != mifare_reader.MI_OK:
+            continue
+
+        # Select the scanned tag
+        mifare_reader.MFRC522_SelectTag(uid)
+
+        (status, data, _) = read_sector(mifare_reader, 1, default_key, uid)
+        print uid
+        last_card = uid
+        mifare_reader.MFRC522_StopCrypto1()
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, end_read)
+    signal.signal(signal.SIGTERM, end_read)
+    signal.signal(signal.SIGQUIT, end_read)
+
+    try:
+        main()
+    finally:
+        GPIO.cleanup()
