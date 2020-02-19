09fab7e8e9feb4669ae65c4d52771d9358571be5
bendem <online@bendem.be>
2020-02-19 18:54:38 +0100

    Cleanup

--- gate-server/src/main/java/be/lghs/gate/GateApplication.java
+++ gate-server/src/main/java/be/lghs/gate/GateApplication.java
@@ -2,11 +2,9 @@ package be.lghs.gate;
 
 import org.eclipse.paho.client.mqttv3.MqttException;
 
-import java.io.IOException;
-
 public class GateApplication {
 
-    public static void main(String... args) throws IOException, MqttException {
+    public static void main(String... args) throws MqttException {
         new GateListener().listen();
     }
 }
